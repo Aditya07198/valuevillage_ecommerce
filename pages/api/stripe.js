5ae0f2033f06c80436618ceff59ad3c2f1120faf
Dhruvpatel3638 <87133344+Dhruvpatel3638@users.noreply.github.com>
2022-08-16 16:07:50 -0400

    Updated stripe.js
    updated stripe.js to collect info from the customer

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
             submit_type: 'pay',
             mode: 'payment',
             payment_method_types: ['card'],
-            billing_address_collection: 'auto',
+            shipping_address_collection: {
+              allowed_countries: ['US', 'CA'],
+            },
             shipping_options: [
               { shipping_rate: 'shr_1LQJwdAQmqeXcuMaLbY6S6tg'}
             ],
