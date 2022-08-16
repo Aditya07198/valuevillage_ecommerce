c2249a224ee13d3c0118639c0bc8597c1b91df10
Rutvij0709 <87199147+Rutvij0709@users.noreply.github.com>
2022-08-15 23:02:01 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  future: {
+    webpack5: true,
+  },
+  webpack: function (config, options) {
+    config.experiments = {
+      ...config.experiments,
+      layers:true,
+    };
+    config.resolve = {
+      ...config.resolve,
+
+      fallback: {
+        // fs: false,
+        // net:false,
+        // dns:false,
+        // tls:false,
+        // child_process:false,
+        // async_hooks:false,
+    }
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    target:'node',
+  mode: 'development',
+//   entry: './foo.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'foo.bundle.js',
+//   },
+};
