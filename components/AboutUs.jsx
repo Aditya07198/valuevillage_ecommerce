5baa3f056690977b39d1cb40d4f203ed9de9ad19
Dhruvpatel3638 <87133344+Dhruvpatel3638@users.noreply.github.com>
2022-08-17 15:00:24 -0400

    aboutus.jsk

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -3,46 +3,67 @@ import React from 'react';
 const about=()=>{
     return(
         <><header>
-            <div className='about-header'>
-                <div class="about-title">
-                    <h3>Vintage Value</h3>
-                </div>
-                <div class="about-content">
-                    <h5>who we are</h5>
-                    <p>We are team of creatively diverse. driven. innovative individuals working in various locations from the world.</p>
-                </div>
-            </div>
-        </header>
-        <main>
-        <div className='about-main'>
-            <div class="about-profile">
-                <figure>
-                    <img src="https://vetri-suriya.web.app/devchallenges/team-page/photo1.png" alt=""></img>
-                    <figcaption>Aditya</figcaption>
-                </figure>
-            </div>
-
-                <div class="about-profile">
-                    <figure>
-                    <img src="https://vetri-suriya.web.app/devchallenges/team-page/photo3.png" alt=""></img>
-                        <figcaption>Rutvij</figcaption>
-                    </figure>
+        </header><main>
+        <div class="container">
+            <div class="about"> 
+                <h2>About US</h2>
+                <hr></hr>
+                <p>Our company VintageValue can boast the reputation of the trusted partner known worldwide. We are proud of the uncompromising quality of services we provide. Placing customers’ needs first in everything we do,we continue to grow rapidlyas the industry leader. A more valuable , less invasive way where customers are earned rather than bought . We're obsessively passionate about it , and our mission is to surge the importance and value of the ancient antiques which were considered as an asset long years back. A great collection and apprentice with best rpices going in the market can be found at our place.
+</p>
                 </div>
 
-                <div class="about-profile">
-                    <figure>
-                    <img src="https://vetri-suriya.web.app/devchallenges/team-page/photo5.png" alt=""></img>
-                        <figcaption>Dhruv</figcaption>
-                    </figure>
-                </div>
+            
+       <div class="team">
+      <h1> Our Team</h1>
+      <hr></hr>
+          
+      <div  class="card">
+         <div class="circle-container">
+            <img src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"></img>
+         </div>
+         <h2>Yagna</h2>
+         <h4>Web Designer</h4>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+          
+          
+          
+      <div class="card">
+         <div class="circle-container">
+         <img src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"></img>
+         </div>
+         <h2>Aditya</h2>
+         <h4>Backend Developer</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+          
+          
+          
+      <div class="card">
+         <div class="circle-container">
+         <img src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"></img>
+         </div>
+         <h2>Rutvij</h2>
+         <h4>Team Leader</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+          
+          
+          
+      <div class="card">
+        <div class="circle-container">
+        <img src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"></img>
+         </div>
+         <h2>Dhruv</h2>
+         <h4>Backend Developer</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+          
+          
+   </div>
 
-                <div class="about-profile">
-                    <figure>
-                    <img src="https://vetri-suriya.web.app/devchallenges/team-page/photo4.png" alt=""></img>
-                        <figcaption>Yagna</figcaption>
-                    </figure>
-                </div>
             </div>
+            
             </main></>
     )
 }
