91a12086a126f7c336f6157f43a4887e232d6747
Rutvij0709 <87199147+Rutvij0709@users.noreply.github.com>
2022-08-17 18:34:31 -0400

    Update ContactForm.js

--- pages/ContactForm.js
+++ pages/ContactForm.js
@@ -34,10 +34,7 @@ export default function Home() {
 console.log("errors",errors)
   return (
     
-    <div className="heading-contact">
-      <h2>Contact Us</h2>
-      <p>Contact </p>
-    </div>
+    
     <div className="py-16 bg-gray-50 px-4 sm:px-6 flex justify-center items-center y">
       <div className="mx-auto w-full rounded-xl bg-white p-8  ">
         <form
