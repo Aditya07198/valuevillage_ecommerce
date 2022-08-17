6b6ba48b888b5382574e09e053c9e70def0e9ee0
Rutvij0709 <87199147+Rutvij0709@users.noreply.github.com>
2022-08-17 17:57:16 -0400

    Update ContactForm.js

--- pages/ContactForm.js
+++ pages/ContactForm.js
@@ -40,43 +40,47 @@ console.log("errors",errors)
           onSubmit={handleSubmit(onSubmitForm)}
           className="mx-auto  shadow p-3 yy"
           >
-          <div className="pb-2 d-flex justify-content-between "   >
-            <label style={{ width:"30%" }} for="name" >
-              Full name:-
-            </label>
+//           <div className="pb-2 d-flex justify-content-between "   >
+             <div className="form-group">
+//             <label style={{ width:"30%" }} for="name" >
+//               Full name:-
+//             </label>
             
-            <input className="text-start w-full" style={{ width: "70%" }}  {...register('name', { required: 'Full Name is required' , maxLength: 30 })}/>
+            <input {...register('name', { required: 'Full Name is required' , maxLength: 30 })} placeholder="Enter your Name"/>
             </div>
           <span className="text-red-400 text-sm py-2" style={{ color:"red" }} >
               {errors?.name?.message}
             </span>
-          <div className="pb-2 d-flex justify-content-between "   >
-            <label style={{ width:"30%" }} for="email" >
-              Email:-
-            </label>
-            <input className="text-start w-full" style={{ width: "70%" }}  {...register('email',{ required: 'You must enter your valid Email address',pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})} />
+//           <div className="pb-2 d-flex justify-content-between "   >
+//             <label style={{ width:"30%" }} for="email" >
+//               Email:-
+//             </label>
+            <div className="form-group">
+              <input {...register('email',{ required: 'You must enter your valid Email address',pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})} placeholder="Enter your Email" />
             </div>
 
           <span className="text-red-400 text-sm py-2" style={{ color:"red" }} >
               {errors?.email?.message}
             </span>
-          <div className="pb-2 d-flex justify-content-between "   >
-            <label style={{ width:"30%" }} for="phone" >
-              Phone:-
-            </label>
-            <input className="text-start w-full" style={{ width: "70%" }}  {...register('phone',{ required: 'Phone number is required'})} />
+//           <div className="pb-2 d-flex justify-content-between "   >
+//             <label style={{ width:"30%" }} for="phone" >
+//               Phone:-
+//             </label>
+            <div className="form-group">
+              <input {...register('phone',{ required: 'Phone number is required'})} placeholder="Enter your Phone Number"/>
             </div>
 
           <span className="text-red-400 text-sm py-2" style={{ color:"red" }} >
             {errors?.phone?.message}
             </span>
-          <div className="pb-2 d-flex justify-content-between "  >
-            <label style={{ width:"30%" }} for="message" >
-              Message:-
-            </label>
+//           <div className="pb-2 d-flex justify-content-between "  >
+//             <label style={{ width:"30%" }} for="message" >
+//               Message:-
+//             </label>
             
           <div style={{ width: "70%" }}  >
-          <textarea style={{ width:"100%" }} {...register('message',{ required: 'Message is required', minLength:10})} />
+          <div className="form-group">
+            <textarea {...register('message',{ required: 'Message is required', minLength:10})} placeholder="Message type here" />
           </div>
 
 </div>
